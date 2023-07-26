89ec752933ccf05aa7b5b46e43c6f72a2eeb304a
sarvar <kingsarvar.1998@gmail.com>
2023-07-26 21:35:55 +0500

    FirstCommit

---
+++ BankAccount/Account.cs
@@ -0,0 +1,32 @@
+﻿namespace BankAccount;
+
+public delegate void AccountHandler(string message); 
+public class Account
+{
+    private int sum { get; set; } //Переменная для хранения суммы
+    // Создаем переменную делегата
+    private AccountHandler? taken;
+    // через конструктор устанавливается начальная сумма на счете
+    public Account(int sum) => this.sum = sum;
+    // добавить средства на счет
+    public void Add(int sum) => this.sum += sum;
+    // Регистрируем делегат
+    public void RegisterHandler(AccountHandler del)
+    {
+        taken = del;
+    }
+    // взять деньги с счета
+    public void Take(int sum)
+    {
+        // берем деньги, если на счете достаточно средств
+        if (this.sum >= sum)
+        {
+            this.sum -= sum;
+            taken?.Invoke("Со счета списано");
+        }
+        else
+        {
+            taken?.Invoke($"не достаточно средств {this.sum}$");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Channels;
+using BankAccount;
+
+#region MyRegion
+// создаем банковский счет
+Account account = new Account(200);
+// Добавляем в делегат ссылку на метод PrintSimpleMessage
+account.RegisterHandler(PrintSimpleMessage);
+// Два раза подряд пытаемся снять деньги
+account.Take(100);;
+account.Take(150);;
+
+void PrintSimpleMessage(string message)=>Console.WriteLine(message);
+#endregion
